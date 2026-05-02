a1068e9070d19b6b03e2cabe8674d157170bc4a9
depo0749 <depo0749@gmail.com>
2026-05-02 15:02:12 +0300

    Add KutuphaneMVC2/KutuphaneMVC2/Migrations/20250424193212_AddOduncModel.cs

---
+++ KutuphaneMVC2/KutuphaneMVC2/Migrations/20250424193212_AddOduncModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KutuphaneMVC2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOduncModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Oduncler",
+                columns: table => new
+                {
+                    OduncId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    KitapId = table.Column<int>(type: "INTEGER", nullable: false),
+                    KullaniciId = table.Column<int>(type: "INTEGER", nullable: false),
+                    OduncAlmaTarihi = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    IadeTarihi = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    GerceklesenIadeTarihi = table.Column<DateTime>(type: "TEXT", nullable: true),
+                    TeslimEdildi = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Notlar = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Oduncler", x => x.OduncId);
+                    table.ForeignKey(
+                        name: "FK_Oduncler_Kitaplar_KitapId",
+                        column: x => x.KitapId,
+                        principalTable: "Kitaplar",
+                        principalColumn: "KitapId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Oduncler_Kullanicilar_KullaniciId",
+                        column: x => x.KullaniciId,
+                        principalTable: "Kullanicilar",
+                        principalColumn: "KullaniciId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Oduncler_KitapId",
+                table: "Oduncler",
+                column: "KitapId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Oduncler_KullaniciId",
+                table: "Oduncler",
+                column: "KullaniciId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Oduncler");
+        }
+    }
+}
